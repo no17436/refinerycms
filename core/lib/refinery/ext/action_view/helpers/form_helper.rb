ebb98b775c81c4783a673f7d5dd94f94b51aed95
Philip Arndt <git@arndt.io>
2014-11-16 13:06:58 -0800

    Removed unneeded assignment; added parentheses to method signature.

--- core/lib/refinery/ext/action_view/helpers/form_helper.rb
+++ core/lib/refinery/ext/action_view/helpers/form_helper.rb
@@ -7,14 +7,14 @@ ActionView::Helpers::FormHelper.module_eval do
   end
 
 
-  def label_humanize_text method, options = {}
+  def label_humanize_text(method, options = {})
     object = options[:object]
 
     content ||= if object && object.class.respond_to?(:human_attribute_name)
       object.class.human_attribute_name(method)
     end
 
-    content ||= method.humanize
+    content || method.humanize
   end
 
 end
